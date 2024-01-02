728a76c7417ce571847ff56f839f604e5116832f
alikanan <a.kanan2222@gmail.com>
2024-01-02 13:12:56 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Nav from "./components/nav";
+function App() {
+
+  return (
+    <div className="App">
+      <Nav/>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Logo from '../assets/images/logo.svg'
+
+function Nav() {
+    return ( <nav className="nav-wrapper">
+        <div className="nav-content">
+            <ul className="list-styled">
+                <li>
+                    <img src={Logo} alt="Apple"/>
+                </li>
+            </ul>
+        </div>
+    </nav> );
+}
+
+export default Nav;
