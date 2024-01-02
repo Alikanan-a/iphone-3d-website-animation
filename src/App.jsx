b5b52e0a051bff97fd15b4f55cfa75b800a50799
alikanan <a.kanan2222@gmail.com>
2024-01-02 14:29:29 +0330

    add  jumbotron component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Nav from "./components/nav";
+import Jumbotron from "./components/Jumbotron";
 function App() {
 
   return (
     <div className="App">
       <Nav/>
+      <Jumbotron/>
     </div>
   );
 }
---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Iphone from "../assets/images/iphone-14.jpg";
+import HoldingIphone from '../assets/images/iphone-hand.png'
+function Jumbotron() {
+  return (
+    <div className="jumbotron-section wrapper">
+      <h2 className="title">New</h2>
+      <img className="logo" src={Iphone} alt="Iphone 14 pro" />
+      <p className="text">Big and Bigger. </p>
+      <span className="description">from $1.63/mo . for 24mo . 999</span>
+      <ul className="links">
+        <li>
+          <button className="button">buy</button>
+        </li>
+        <li>
+          <a className="link">Learn more</a>
+        </li>
+      </ul>
+      <img className="iphone-img" src={HoldingIphone} alt="iphone"/>
+    </div>
+  );
+}
+
+export default Jumbotron;
