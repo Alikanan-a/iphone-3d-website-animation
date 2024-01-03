8e7eac8698edd565bb8547f105f823b891805187
alikanan <a.kanan2222@gmail.com>
2024-01-03 23:50:56 +0330

    add gsap animation

--- src/components/WebgiViewer.jsx
+++ src/components/WebgiViewer.jsx
@@ -24,9 +24,21 @@ import {
 } from "webgi";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { scrollAnimation } from "../lib/scroll-animation";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function WebgiViewer() {
   const canvasRef = useRef(null);
+
+  const memoizedScrollAnimation = useCallback(
+    (position , target , onUpdate) => {
+    if (position && target && onUpdate ) {
+        scrollAnimation(position , target , onUpdate);
+    }
+  } , []
+  )
+
 
   const setupViewer =  useCallback(async () => {
     const viewer = new ViewerApp({
@@ -56,14 +68,22 @@ function WebgiViewer() {
 
     viewer.scene.activeCamera.setCameraOptions({controlsEnabled:false});
     window.scrollTo(0,0);
+
     let needsUpdate = true ;
+
+    const onUpdate = () => {
+        needsUpdate = true ; 
+        viewer.setDirty();
+    }
+
     viewer.addEventListener('preFrame' , () => {
         if (needsUpdate) {
             camera.positionTargetUpdated(true);
             needsUpdate = false; 
         }
        
-    })
+    });
+    memoizedScrollAnimation(position , target , onUpdate)
   }, []);
   
   useEffect(() => {
